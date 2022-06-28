59ef29e5536d37d081c96f4ecc678de20f098a8d
xxj <861479614@qq.com>
2022-06-28 18:14:38 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,112 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <GlobalHeader :user="currentUser" />
+    <!-- <ColumnList :list="list" /> -->
+    <form action="">
+      <div class="mb-3">
+        <label
+          for="exampleInputEmail1"
+          class="form-label"
+        >邮箱地址</label>
+        <input
+          type="text"
+          class="form-control"
+          id="exampleInputEmail1"
+          v-model="emailRef.val"
+          @blur="validateEmail"
+        >
+        <div
+          class="form-text"
+          v-if="emailRef.error"
+        >{{emailRef.message}}</div>
+      </div>
+      <div class="mb-3">
+        <label
+          for="exampleInputPassword1"
+          class="form-label"
+        >密码</label>
+        <input
+          type="password"
+          class="form-control"
+          id="exampleInputPassword1"
+        >
+      </div>
+    </form>
+
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+// import ColumnList, { ColumnProps } from "@/components/ColumnList.vue";
+import GlobalHeader, { UserProps } from "@/components/GlobalHeader.vue";
+import { defineComponent, reactive } from "vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+// const testData: ColumnProps[] = [
+//   {
+//     id: 1,
+//     title: "test1的专栏",
+//     description: "这是的test1专栏，有一段非常有意思的简介，可以更新一下欧",
+//     avatar:
+//       "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100",
+//   },
+//   {
+//     id: 2,
+//     title: "test2的专栏",
+//     description: "这是的test2专栏，有一段非常有意思的简介，可以更新一下欧",
+//     avatar: "",
+//   },
+//   {
+//     id: 3,
+//     title: "test3的专栏",
+//     description: "这是的test3专栏，有一段非常有意思的简介，可以更新一下欧",
+//     avatar:
+//       "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100",
+//   },
+//   {
+//     id: 4,
+//     title: "test4的专栏",
+//     description: "这是的test4专栏，有一段非常有意思的简介，可以更新一下欧",
+//     avatar: "",
+//   },
+// ];
 
+const currentUser: UserProps = {
+  isLogin: true,
+  name: "viking",
+};
+// 邮箱验证
+const emailReg =
+  /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.(com|cn|net)$/;
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-})
+    // ColumnList,
+    GlobalHeader,
+  },
+  setup() {
+    const emailRef = reactive({
+      val: "",
+      error: false,
+      message: "",
+    });
+    const validateEmail = () => {
+      if (emailRef.val.trim() === "") {
+        emailRef.error = true;
+        emailRef.message = "can not be empty";
+      } else if (!emailReg.test(emailRef.val)) {
+        emailRef.error = true;
+        emailRef.message = "should be valid email";
+      }
+    };
+    return {
+      // list: testData,
+      currentUser,
+      emailRef,
+      validateEmail,
+    };
+  },
+});
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
